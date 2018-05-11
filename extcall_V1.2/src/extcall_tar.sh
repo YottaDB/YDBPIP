7f1594505c0adb0fcd22ac6572fcc8146ad6e6b6
Christopher Edwards <cje1985@gmail.com>
2018-05-11 13:51:04 -0400

    Remove execute bits on files and fix shell scripts to be executable
    *.err files had an errant execute bit set
    
    *.sh files didn't have shebang lines that allow the main build.sh
    script to execute child scripts
    
    extcallversion binary was accidentally committed when it shouldn't be

--- extcall_V1.2/src/extcall_tar.sh
+++ extcall_V1.2/src/extcall_tar.sh
@@ -1,5 +1,6 @@
+#!/bin/sh
 rm -f extcall_linux_64.*
 
-tar -cf extcall_linux_64.tar Makefile asc2ebc.c asc2ebc.o atmutils.c atmutils.o devutils.c devutils.o ebc2asc.c ebc2asc.o elfhash.c elfhash.o expsca.c expsca.o extcall.h extcall.sl extcall.xc getquijob.c gtmxc_types.h scatype.h libextcall.a lnx.c lnx.o logsca.c logsca.o md5.h md5c.c md5c.o password.c pidutils.c pidutils.o readport.c readport.o remote.c remote.o rtb.c rtb.o rtbar.c rtbar.o rules.mk scamd5.c scamd5.h scamd5.o sigutils.c slibrule.mk string.c string.o sysutils.c sysutils.o unpack.c unpack.o unpack2.c unpack2.o utils.c utils.o xor.c xor.o extcallversion version.c version.mk
+tar -cf extcall_linux_64.tar Makefile asc2ebc.c asc2ebc.o atmutils.c atmutils.o devutils.c devutils.o ebc2asc.c ebc2asc.o elfhash.c elfhash.o expsca.c expsca.o extcall.h extcall.sl extcall.xc getquijob.c gtmxc_types.h scatype.h lnx.c lnx.o logsca.c logsca.o md5.h md5c.c md5c.o password.c pidutils.c pidutils.o readport.c readport.o remote.c remote.o rtb.c rtb.o rtbar.c rtbar.o rules.mk scamd5.c scamd5.h scamd5.o sigutils.c slibrule.mk string.c string.o sysutils.c sysutils.o unpack.c unpack.o unpack2.c unpack2.o utils.c utils.o xor.c xor.o extcallversion version.c version.mk
 
 gzip -9 *.tar
