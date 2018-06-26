a3f38a9720508d8c6c7ac96715acbb491cb3585d
Christopher Edwards <Christopher.Edwards@krminc.com>
2018-06-26 11:51:39 -0400

    Remove old code from testing framework

--- p/KBBOSQLT.m
+++ p/KBBOSQLT.m
@@ -7,70 +7,17 @@ TEST
  ;
  ; Patient File Tests
 PATIENT ; @TEST
- ;D TESTSQL(2,1)
  D VERIFY(2,1)
  QUIT
  ;
  ; Person File Tests
 PERSON ; @TEST
- ;D TESTSQL(200,5)
  D VERIFY(200,5)
  QUIT
  ;
  ;
- ; All SQL Tests follow the same order of operations - so this method can
- ; test any FileMan mapped SQL table.
-TESTSQL(FILE,IEN)
- N SQLPAR,ERROR,ROWS,SQL,FILENAME,QUERY
- N %DB,%LIBS,%TOKEN,%MSGID,ER,I,J,pslTbl,fsn,vobj,RM
- S SQLPAR("ROWS")=1
- S FILENAME=$$FNB^DMSQU(FILE)
- S QUERY="SELECT "_$$LIST^SQLDD(FILENAME)_" FROM "_FILENAME
- I IEN S QUERY=QUERY_" WHERE IEN="_IEN
- S ERROR=$$^SQL(QUERY,.SQLPAR,,.ROWS)
- D ASSERT(0,ERROR,"Error status code set from $$^SQL")
- D ASSERT(1,$D(ROWS),"No rows returned from query")
- D ARRAYSQL(.SQL,ROWS)
- D ASSERT(SQLPAR("ROWS"),$O(SQL(""),-1),"Too many rows returned")
- D VERIFYSQL(FILE,.SQL)
- QUIT
- ; Converts ROWS returned by a SQL Query to an array format for easy checking
- ; RETURNS: RETURN(#)=ROW
-ARRAYSQL(RETURN,ROWS)
- N ROW
- F ROW=1:1:$L(ROWS,$C(13,10)) D
- . S RETURN(ROW)=$P(ROWS,$C(13,10),ROW)
- QUIT
- ;
-VERIFYSQL(FILE,SQL,SUBFILE)
- N ROW,ERROR,FIELD,FILENAME,SQLFIELDNAME,FM,SQLIENS,SQLMAP,FIELDNAME,COLUMNS
- N DIC,FMFILE
- ;
- S ROW=0 ; Skip the first SQL row as that is the 0 node which is invalid
- F  S ROW=$O(SQL(ROW)) Q:ROW=""  D
- . K FM,ERROR,SQLIENS
- . S SQLIENS=$P(SQL(ROW),$C(9),1)_","
- . D GETS^DIQ(FILE,SQLIENS,"**","I","FM","ERROR")
- . D ASSERT(0,$D(ERROR),"FileMan error occurred")
- . I $D(ERROR) W "FileMan Error: ",! ZWRITE ERROR QUIT
- . S FMFILE=""
- . S FILENAME=$$FNB^DMSQU(FILE)
- . D GETCOLUMNS(FILENAME)
- . W !
- . F  S FMFILE=$O(FM(FMFILE)) Q:FMFILE=""  D
- . . W "FMFILE: "_FMFILE,!
- . . S FIELD=""
- . . F  S FIELD=$O(FM(FMFILE,SQLIENS,FIELD)) Q:FIELD=""  D
- . . . K SQLMAP,FIELDNAME,SQLFIELDNAME
- . . . ; have to cross walk Fileman and SQL mapping to get field number to piece translation
- . . . ; Get a SQL compatible Column Name for the Field
- . . . S SQLFIELDNAME=$$SQLK^DMSQU($P(^DD(FMFILE,FIELD,0),"^",1),30)
- . . . ; Skip computed fields
- . . . I $P(^DD(FMFILE,FIELD,0),"^",2)["C" Q
- . . . ;
- . . . D ASSERT(FM(FMFILE,SQLIENS,FIELD,"I"),$P(SQL(ROW),$C(9),COLUMNS(SQLFIELDNAME)),"data mismatch between FileMan File: "_FILENAME_" (#"_FMFILE_") Field: "_$P(^DD(FILE,FIELD,0),"^",1)_" (#"_FIELD_") and SQL "_SQLFIELDNAME)
- QUIT
- ;
+ ; This is intended to verify any FileMan File including it's SubFiles
+ ; given the root File Number and a given IEN.
 VERIFY(FILE,IEN)
  N ROW,ERROR,FIELD,FILENAME,SQLFIELDNAME,FM,SQLMAP,FIELDNAME,COLUMNS
  N DIC,FMFILE,SIEN,SQLIENS,X1,FLAG
@@ -84,7 +31,6 @@ VERIFY(FILE,IEN)
  S FMFILE=""
  W !
  F  S FMFILE=$O(FM(FMFILE)) Q:FMFILE=""  D
- . W "FMFILE: "_FMFILE,!
  . S FILENAME=$$FNB^DMSQU(FMFILE)
  . D GETCOLUMNS(FILENAME)
  . S SQLIENS=""
@@ -100,7 +46,7 @@ VERIFY(FILE,IEN)
  . . . I 'FLAG S QUERY=QUERY_" WHERE IEN="_$P(SQLIENS,",",SIEN),FLAG=1
  . . . E  I FILENAME="PATIENT_APPOINTMENT" S QUERY=QUERY_" AND APPOINTMENT_DATE_TIME="_$P(SQLIENS,",",SIEN)
  . . . E  S QUERY=QUERY_" AND IEN"_SIEN_"="_$P(SQLIENS,",",SIEN)
- . . W "QUERY: "_QUERY,!
+ . . ;W "QUERY: "_QUERY,!
  . . S ERROR=$$^SQL(QUERY,.SQLPAR,,.ROWS)
  . . D ASSERT(0,ERROR,"Error status code set from $$^SQL")
  . . D ASSERT(1,$D(ROWS),"No rows returned from query")
@@ -120,6 +66,15 @@ VERIFY(FILE,IEN)
  . . . D ASSERT(FM(FMFILE,SQLIENS,FIELD,"I"),$P(SQL(ROW),$C(9),COLUMNS(SQLFIELDNAME)),"data mismatch between FileMan File: "_FILENAME_" (#"_FMFILE_") Field: "_$P(^DD(FMFILE,FIELD,0),"^",1)_" (#"_FIELD_") and SQL "_SQLFIELDNAME)
  QUIT
  ;
+ ; Converts ROWS returned by a SQL Query to an array format for easy checking
+ ; RETURNS: RETURN(#)=ROW
+ARRAYSQL(RETURN,ROWS)
+ N ROW
+ F ROW=1:1:$L(ROWS,$C(13,10)) D
+ . S RETURN(ROW)=$P(ROWS,$C(13,10),ROW)
+ QUIT
+ ;
+ ; Gets columns as defined by DATA-QWIK
 GETCOLUMNS(FILE)
  N SQLFIELDNAME,POSITION,LIST
  S SQLFIELDNAME=""
